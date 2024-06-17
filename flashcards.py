0b3cc1b519b20bedab3e80b71e6665674ad708da
apple <paisworya2@gmail.com>
2024-06-17 18:07:00 +0530

    Modified the file

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return "Welcome to my flash card application!"
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
